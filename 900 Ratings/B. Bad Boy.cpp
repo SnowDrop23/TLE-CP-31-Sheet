80e8eb689c1af0ed2d23b68cd18e96fa6f892537
toma <144379705+SnowDrop23@users.noreply.github.com>
2026-01-03 21:33:27 +0600

    Create B. Bad Boy.cpp

---
+++ 900 Ratings/B. Bad Boy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        long long x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        cout << 1 << " " << 1 << " " << x1 << " " << y1 << endl;
+    }
+}
